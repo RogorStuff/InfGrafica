9b6ffbfd9a7ad0f861b8fdc4c8a4ff96167e5cd9
RogorStuff <lacruzrogelio@gmail.com>
2020-10-20 18:59:19 +0200

    cosas

--- P2/Main.cpp
+++ P2/Main.cpp
@@ -1,7 +1,20 @@
-#include "iostream"
 #include "math.h"
+#include "toneMaping.cpp"
+#include <iostream>
+#include <fstream>
+#include <string.h> 
+#include <stdio.h> 
+
 using namespace std;
 
 int main () {
+
+    string fileName = "forest_path.ppm";
+    cout << "Please enter the name of the img: ";
+    //cin >> fileName;
+
+    Image thisImg(fileName);
+
+    
 
 }
---
+++ P2/toneMaping.cpp
@@ -0,0 +1,82 @@
+#include "math.h"
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string.h> 
+#include <stdio.h> 
+
+
+using namespace std;
+
+class Pixel {
+    public:
+        float R;
+        float G;
+        float B;
+        Pixel();
+        Pixel(float Rgb, float rGb, float rgB);
+};
+
+Pixel::Pixel(){
+}
+
+Pixel::Pixel(float Rgb, float rGb, float rgB){
+    this->R=Rgb;
+    this->G=rGb;
+    this->B=rgB;
+}
+
+class Image {
+    public:
+        string formatID;
+        string name;
+        float m;
+        int c;
+        int width, height;
+        vector<Pixel> imageMatrix; //imageMatrix[height][width]
+        Image();
+        Image(string fileName);
+};
+
+Image::Image(){
+}
+
+Image::Image(string fileName){
+    ifstream fileReader(fileName);
+    if (fileReader.is_open()){
+
+        getline(fileReader,formatID);   //Read format identification
+
+        string max;
+        getline(fileReader,max);        //#MAX=1656 pasar a float 1656
+
+        string aux = max.substr(5);
+        m=stof(aux);
+
+        getline(fileReader,name);
+
+        fileReader >> width >> height >> c;
+
+        cout << formatID << endl;
+        cout << max << endl;
+        cout << name << endl;
+        cout << width << " " << height << endl;
+        cout << c << endl;
+
+        imageMatrix.resize(width*height);
+
+        int r,g,b;
+        int total = height*width;
+        for (int i = 0; i < total; i++){
+            fileReader >> r >> g >> b;
+            Pixel pixel(r,g,b);
+            imageMatrix[i]=pixel;
+        }
+
+        cout << "Imagen guardada" << endl;
+    }else{
+        cout << "No se pudo abrir o encontrar la imagen" << endl;
+    }
+}
+
+Image GammaCurve (Image imagen){}
