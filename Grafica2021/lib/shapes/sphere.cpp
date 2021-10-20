abbbcb0b3f215533debdad2e581a0c2da273b17f
RogorStuff <lacruzrogelio@gmail.com>
2021-10-20 17:29:27 +0200

    funcion de intersectar esfera puesta, no se si va

--- Grafica2021/lib/shapes/sphere.cpp
+++ Grafica2021/lib/shapes/sphere.cpp
@@ -18,3 +18,30 @@ vec3 sphere::getCenter(){
 float sphere::getRadius(){
     return this->radius;
 }
+
+bool sphere::ray_intersect(ray& r, colour& tono, float& distancia){
+    vec3 d = r.direccion;
+    vec3 o = r.origen;
+
+    vec3 oc = this->center;
+
+    float t_min = -1;
+    float t_max = 999999999999999;
+
+    float a = dot(d, d);
+    float b = dot(oc, d);
+    float c = dot(oc, oc) - (this->radius * this->radius);
+    float discriminant = (b * b) - ((a * c));
+    if (discriminant > 0) {
+        float temp = fabs((-b - sqrt(discriminant)) / a);
+        float temp2 = fabs((-b + sqrt(discriminant)) / a);
+        tono = this->color;
+        if (temp > temp2) {
+            distancia = temp2;
+            return true;
+        }
+        distancia = temp;
+        return true;
+    } 
+    return false;
+};
--- Grafica2021/lib/shapes/sphere.hpp
+++ Grafica2021/lib/shapes/sphere.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../math/vec3.cpp"
+#include "../scene/ray.cpp"
 #include "colour.hpp"
 #include "math.h"
 #include <vector>
@@ -23,6 +24,7 @@ class sphere {
         sphere(vec3 center_, float radius_, colour color_);
         vec3 getCenter();
         float getRadius();
+        bool ray_intersect(ray& r, colour& tono, float& distancia);
 };
 
 #endif
